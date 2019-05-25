81b7bd59610a8f5fdcf4e12e2854c0c12246dcb6
Jane Jeon <JaneJeon9719@gmail.com>
2019-05-24 23:29:11 -0400

    job-ify

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 require("express-async-errors")
 require("dotenv").config()
 
+// schedule jobs
+const cron = require("node-cron")
+cron.schedule("* * */6 * * *", require("./jobs/listserv")) // every 6 hours
+
 const express = require("express")
 const app = express()
 const hbs = require("hbs")
 const path = require("path")
+const debug = require("debug")("dartcal:app")
 
-hbs.registerPartials(path.join(__dirname, "views/partials"))
+// hbs.registerPartials(path.join(__dirname, "views/partials"))
 
 app
   .use(require("helmet"))
-  .use(express.static(path.join(__dirname, "public")))
-  .set("views", path.join(__dirname, "views"))
-  .set("view engine", "hbs")
+  // .use(express.static(path.join(__dirname, "public")))
+  // .set("views", path.join(__dirname, "views"))
+  // .set("view engine", "hbs")
   .get("/", async (req, res) => {
     //
   })
 
 app.listen(process.env.PORT, err => {
   if (err) console.error(err)
+  else debug("Server started!")
 })
--- jobs/listserv.js
+++ jobs/listserv.js
@@ -1,30 +1,24 @@
-require("dotenv").config()
 const NaturalLanguageUnderstandingV1 = require("ibm-watson/natural-language-understanding/v1")
 const nlu = new NaturalLanguageUnderstandingV1({
   version: "2019-04-02",
   url: "https://gateway.watsonplatform.net/natural-language-understanding/api",
   iam_apikey: process.env.IAM_API_KEY
 })
-const event = require("./lib/event")
-const debug = require("debug")("dartcal:test")
-
 const Parser = require("rss-parser")
+const event = require("../lib/event")
+const debug = require("debug")("dartcal:jobs:listserv")
 
-const hello = async () => {
+module.exports = async () => {
   const parser = new Parser()
   const feed = await parser.parseURL(process.env.EVENT_FEED)
 
-  /* Exceptions to note:
-   * - emails with "empty" content; e.g. "[cid:fe9c1a86-21da-415f-a5a5-f821f29bce9d] "
-   * - replies w/o context; e.g. "*clarification because the date was only in the subject line* The show is on
-   *  Monday :-) see you there"
-   */
-  for (let i = 0; i < 10; i++) {
-    const item = feed.items[i]
+  debug("Checking", feed.title)
+
+  feed.items.forEach(item => {
     debug("item: %o", item)
 
     const cleaned = event.clean(item)
-    if (!cleaned) continue
+    if (!cleaned) return
 
     const result = await nlu.analyze({
       text: event.clean(item),
@@ -38,7 +32,7 @@ const hello = async () => {
 
     const info = event.extract(result.entities, item)
     debug("extracted info: %o", info)
-  }
-}
 
-hello().catch(console.error)
+    // TODO: do something with this info
+  })
+}
